a440856a89c19637bdcf94dac3fbff58ec47a9f3
Cozmo o-o <87493302+Cozmo100@users.noreply.github.com>
2022-10-10 15:38:12 -0500

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,11 @@
+wun = localStorage.getItem("USERNAME");
+document.getElementById('welcome').innerHTML = "Welcome " + wun;
+//ADD YOUR FIREBASE LINKS HERE
+
+function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+       Room_names = childKey;
+      //Start code
+
+      //End code
+      });});}
+getData();
